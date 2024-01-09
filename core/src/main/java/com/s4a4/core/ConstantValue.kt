1b38e9bd2c150b07808196ffa1d1d220f2e78b2b
S4a5 <rin.islamov@bk.ru>
2024-01-09 15:36:13 +0600

    1231321

--- core/src/main/java/com/s4a4/core/ConstantValue.kt
+++ core/src/main/java/com/s4a4/core/ConstantValue.kt
@@ -2,8 +2,9 @@ package com.s4a4.core
 
 private const val URL_SERVER = "45.132.19.24:8080"
 private const val URL_LOCAL = "192.168.1.89:8080"
+private const val URL_K_LOCAL = "192.168.40.156:8080"
 
-const val UrlServer = "http://$URL_SERVER"
+const val UrlServer = "http://$URL_K_LOCAL"
 const val UrlImageVoice = "/image/logo/"
 
 const val MIN_LENGHT_SEARCH = 4
